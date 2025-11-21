d81c94a135781f4a9f491a0a08ec028147bcf50c
Lucas Iannitello <lucas.iannitello@protonmail.com>
2025-11-21 18:36:56 -0500

    Implement `RootModel` and `dataclass` patching

--- src/syncwave/sync_model.py
+++ src/syncwave/sync_model.py
@@ -3,10 +3,12 @@ from __future__ import annotations
 import dataclasses as dc
 from abc import ABCMeta, abstractmethod
 from collections.abc import Mapping
+from inspect import isclass
 from typing import Annotated, Any, Generic, TypeVar, Union, final, get_args, get_origin
 from typing_extensions import TypeGuard
 
 from pydantic import BaseModel, RootModel, TypeAdapter, create_model
+from pydantic import dataclasses as pdc
 
 from .reactive import Reactive, atomic
 from .sync_collection import SyncCollection, SyncDict, SyncList, SyncSet
@@ -14,7 +16,7 @@ from .sync_collection import SyncCollection, SyncDict, SyncList, SyncSet
 
 class SyncModelSupportedMeta(ABCMeta):
     def __subclasscheck__(self, subclass: type[Any]) -> bool:
-        if not isinstance(subclass, type):
+        if not isclass(subclass):
             return False
         is_model = any(base in (BaseModel, RootModel) for base in subclass.__mro__)
         is_dc = dc.is_dataclass(subclass)
@@ -38,6 +40,9 @@ class SyncModelSupported(metaclass=SyncModelSupportedMeta):
 
 
 T = TypeVar("T", bound=SyncModelSupported)
+T_BM = TypeVar("T_BM", bound=BaseModel)
+T_RM = TypeVar("T_RM", bound=RootModel)
+T_DC = TypeVar("T_DC")  # dataclass
 
 
 class SyncModel(Generic[T], Reactive):
@@ -52,7 +57,7 @@ class SyncModel(Generic[T], Reactive):
 
 
 def reactive(cls: type[T], /, *, cls_name: str | None = None) -> type[SyncModel[T]]:
-    if not isinstance(cls, type):
+    if not isclass(cls):
         raise TypeError(f"{cls} is not a valid type.")
 
     is_base_model = any(base is BaseModel for base in cls.__mro__)
@@ -70,7 +75,7 @@ def reactive(cls: type[T], /, *, cls_name: str | None = None) -> type[SyncModel[
         raise TypeError(f"Class '{cls.__name__}' is not a valid SyncModelSupported.")
 
 
-def patch_base_model(cls: type[T], cls_name: str) -> type[SyncModel[T]]:
+def patch_base_model(cls: type[T_BM], cls_name: str) -> type[SyncModel[T_BM]]:
     if cls.model_config.get("frozen"):
         raise ValueError(f"'{cls.__name__}' is frozen and cannot be made reactive.")
 
@@ -85,11 +90,12 @@ def patch_base_model(cls: type[T], cls_name: str) -> type[SyncModel[T]]:
     original_delattr = cls.__delattr__
 
     def __syncwave_update__(
-        self: SyncModel[T], new: SyncModel[T] | Mapping[str, Any]
+        self: SyncModel[T_BM],
+        new: SyncModel[T_BM] | T_BM | Mapping[str, Any],
     ) -> None:
         new = self.__type_adapter.validate_python(new)
 
-        for name in self.model_fields:
+        for name in cls.model_fields:
             new_value = getattr(new, name, None)
 
             if name not in children_type_adapter:
@@ -108,7 +114,7 @@ def patch_base_model(cls: type[T], cls_name: str) -> type[SyncModel[T]]:
                 original_setattr(self, name, new_value)
 
     @atomic
-    def __setattr__(self: SyncModel[T], name: str, new_value: Any) -> None:
+    def __setattr__(self: SyncModel[T_BM], name: str, new_value: Any) -> None:
         old_value = getattr(self, name, None)
 
         if name not in children_type_adapter:
@@ -129,7 +135,7 @@ def patch_base_model(cls: type[T], cls_name: str) -> type[SyncModel[T]]:
         self.__syncwave_on_change__()
 
     @atomic
-    def __delattr__(self: SyncModel[T], name: str) -> None:
+    def __delattr__(self: SyncModel[T_BM], name: str) -> None:
         old_value = getattr(self, name, None)
 
         if name in children_type_adapter:
@@ -139,22 +145,191 @@ def patch_base_model(cls: type[T], cls_name: str) -> type[SyncModel[T]]:
         original_delattr(self, name)
         self.__syncwave_on_change__()
 
+    def __syncwave_abc_marker__(self: SyncModel[T_BM]) -> None:
+        pass
+
     new_cls_dict = {
         "__syncwave_update__": __syncwave_update__,
         "__setattr__": __setattr__,
         "__delattr__": __delattr__,
+        "__syncwave_abc_marker__": __syncwave_abc_marker__,
+        "__module__": cls.__module__,
     }
 
     new_cls = create_model(cls_name, __base__=(cls, SyncModel), **new_cls_dict)
     return new_cls
 
 
-def patch_root_model(cls: type[T], cls_name: str) -> type[SyncModel[T]]:
-    pass
+def patch_root_model(cls: type[T_RM], cls_name: str) -> type[SyncModel[T_RM]]:
+    if cls.model_config.get("frozen"):
+        raise ValueError(f"'{cls.__name__}' is frozen and cannot be made reactive.")
+
+    root_type_adapter: TypeAdapter | None = None
+    root_field = cls.model_fields["root"]
+    if drill_field("root", root_field.annotation, reactive_allowed=True):
+        if root_field.frozen:
+            raise TypeError("Field 'root': frozen fields cannot be reactive.")
+        root_type_adapter = TypeAdapter(root_field.annotation)
+
+    original_setattr = cls.__setattr__
+    original_delattr = cls.__delattr__
+
+    def __syncwave_update__(self: SyncModel[T_RM], new: Any) -> None:
+        new = self.__type_adapter.validate_python(new)
+        new_value = new.root
+
+        if root_type_adapter is None:
+            original_setattr(self, "root", new_value)
+            return
+
+        old_value = self.root
+        old_is_reactive = isinstance(old_value, Reactive)
+        safe_to_update = old_is_reactive and isinstance(new_value, type(old_value))
+        if safe_to_update:
+            old_value.__syncwave_update__(new_value)
+            original_setattr(self, "root", old_value)
+        else:
+            if old_is_reactive:
+                old_value.__syncwave_live__ = False
+            original_setattr(self, "root", new_value)
+
+    @atomic
+    def __setattr__(self: SyncModel[T_RM], name: str, new_value: Any) -> None:
+        if name != "root" or root_type_adapter is None:
+            original_setattr(self, name, new_value)
+            self.__syncwave_on_change__()
+            return
+
+        old_value = self.root
+        new_value = root_type_adapter.validate_python(new_value)
+
+        old_is_reactive = isinstance(old_value, Reactive)
+        safe_to_update = old_is_reactive and isinstance(new_value, type(old_value))
+        if safe_to_update:
+            old_value.__syncwave_update__(new_value)
+            original_setattr(self, "root", old_value)
+        else:
+            if old_is_reactive:
+                old_value.__syncwave_live__ = False
+            original_setattr(self, "root", new_value)
+        self.__syncwave_on_change__()
+
+    @atomic
+    def __delattr__(self: SyncModel[T_RM], name: str) -> None:
+        if name != "root" or root_type_adapter is None:
+            original_delattr(self, name)
+            self.__syncwave_on_change__()
+            return
+
+        old_value = self.root
+        old_is_reactive = isinstance(old_value, Reactive)
+        if old_is_reactive:
+            old_value.__syncwave_live__ = False
+        original_delattr(self, name)
+        self.__syncwave_on_change__()
+
+    def __syncwave_abc_marker__(self: SyncModel[T_RM]) -> None:
+        pass
+
+    new_cls_dict = {
+        "__syncwave_update__": __syncwave_update__,
+        "__setattr__": __setattr__,
+        "__delattr__": __delattr__,
+        "__syncwave_abc_marker__": __syncwave_abc_marker__,
+        "__module__": cls.__module__,
+    }
+
+    new_cls = create_model(cls_name, __base__=(cls, SyncModel), **new_cls_dict)
+    return new_cls
 
 
-def patch_dataclass(cls: type[T], cls_name: str) -> type[SyncModel[T]]:
-    pass
+def patch_dataclass(cls: type[T_DC], cls_name: str) -> type[SyncModel[T_DC]]:
+    if cls.__dataclass_params__.frozen:
+        raise ValueError(f"'{cls.__name__}' is frozen and cannot be made reactive.")
+
+    if not pdc.is_pydantic_dataclass(cls):
+        cls = pdc.dataclass(cls)
+
+    children_type_adapter = {}
+    for name, field in cls.__pydantic_fields__.items():
+        if drill_field(name, field.annotation, reactive_allowed=True):
+            if field.frozen:
+                raise TypeError(f"Field '{name}': frozen fields cannot be reactive.")
+            children_type_adapter[name] = TypeAdapter(field.annotation)
+
+    original_setattr = cls.__setattr__
+    original_delattr = cls.__delattr__
+
+    def __syncwave_update__(
+        self: SyncModel[T_DC],
+        new: SyncModel[T_DC] | T_DC | Mapping[str, Any],
+    ) -> None:
+        new = self.__type_adapter.validate_python(new)
+
+        for name in cls.__pydantic_fields__:
+            new_value = getattr(new, name, None)
+
+            if name not in children_type_adapter:
+                original_setattr(self, name, new_value)
+                continue
+
+            old_value = getattr(self, name, None)
+            old_is_reactive = isinstance(old_value, Reactive)
+            safe_to_update = old_is_reactive and isinstance(new_value, type(old_value))
+            if safe_to_update:
+                old_value.__syncwave_update__(new_value)
+                original_setattr(self, name, old_value)
+            else:
+                if old_is_reactive:
+                    old_value.__syncwave_live__ = False
+                original_setattr(self, name, new_value)
+
+    @atomic
+    def __setattr__(self: SyncModel[T_DC], name: str, new_value: Any) -> None:
+        old_value = getattr(self, name, None)
+
+        if name not in children_type_adapter:
+            original_setattr(self, name, new_value)
+            self.__syncwave_on_change__()
+            return
+
+        new_value = children_type_adapter[name].validate_python(new_value)
+        old_is_reactive = isinstance(old_value, Reactive)
+        safe_to_update = old_is_reactive and isinstance(new_value, type(old_value))
+        if safe_to_update:
+            old_value.__syncwave_update__(new_value)
+            original_setattr(self, name, old_value)
+        else:
+            if old_is_reactive:
+                old_value.__syncwave_live__ = False
+            original_setattr(self, name, new_value)
+        self.__syncwave_on_change__()
+
+    @atomic
+    def __delattr__(self: SyncModel[T_DC], name: str) -> None:
+        old_value = getattr(self, name, None)
+
+        if name in children_type_adapter:
+            old_is_reactive = isinstance(old_value, Reactive)
+            if old_is_reactive:
+                old_value.__syncwave_live__ = False
+        original_delattr(self, name)
+        self.__syncwave_on_change__()
+
+    def __syncwave_abc_marker__(self: SyncModel[T_DC]) -> None:
+        pass
+
+    new_cls_dict = {
+        "__syncwave_update__": __syncwave_update__,
+        "__setattr__": __setattr__,
+        "__delattr__": __delattr__,
+        "__syncwave_abc_marker__": __syncwave_abc_marker__,
+        "__module__": cls.__module__,
+    }
+
+    new_cls = type(cls_name, (cls, SyncModel), new_cls_dict)
+
+    return pdc.dataclass(new_cls)
 
 
 def drill_field(name: str, tp: Any, reactive_allowed: bool) -> TypeGuard[Reactive]:
@@ -166,7 +341,7 @@ def drill_field(name: str, tp: Any, reactive_allowed: bool) -> TypeGuard[Reactiv
     args = get_args(tp)
     len_args = len(args)
 
-    if isinstance(origin, type):
+    if isclass(origin):
         if issubclass(origin, SyncModel):
             raise TypeError(f"Field '{name}': SyncModel types cannot be nested.")
 
