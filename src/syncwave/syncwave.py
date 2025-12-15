231e51f3dd96b09b196a99dcdc56b0aa50445898
Lucas Iannitello <lucas.iannitello@protonmail.com>
2025-12-15 16:44:09 -0500

    Decommission `context.py`

--- src/syncwave/syncwave.py
+++ src/syncwave/syncwave.py
@@ -4,18 +4,27 @@ import contextlib
 from collections.abc import Callable, Iterator, MutableMapping
 from copy import deepcopy
 from dataclasses import dataclass
+from inspect import isclass
 from pathlib import Path
 from threading import RLock
-from typing import Any, TypeVar, overload
+from typing import Annotated, Any, Union, get_args, get_origin, overload
 
 from pydantic import PydanticSchemaGenerationError, TypeAdapter
 
 from .io import io
-from .reactive import ReactiveBase
-from .sync_model import SyncModel, SyncModelSupported
+from .reactive import Context, ContextMap, Reactive
+from .sync_collection import (
+    KT,
+    VT,
+    SyncDict,
+    SyncDictCtx,
+    SyncList,
+    SyncListCtx,
+    SyncSet,
+    SyncSetCtx,
+)
+from .sync_model import SyncModel, T
 from .watcher import watcher
-
-T = TypeVar("T", bound=SyncModelSupported)
 
 default_provider = {"array": list, "object": dict, "string": str, "null": lambda: None}
 _UNINITIALIZED = object()
@@ -38,7 +47,7 @@ class _RMetadata(_Metadata):
 
 
 # Has to be thread-safe, this is a temporary solution just to start the implementation.
-class Syncwave(MutableMapping[str, Any], ReactiveBase):
+class Syncwave(MutableMapping[str, Any]):
     def __init__(self, stores_dir: str | Path = "") -> None:
         stores_dir = (
             io.get_root_dir() / "syncstores"
@@ -152,3 +161,118 @@ class Syncwave(MutableMapping[str, Any], ReactiveBase):
             self.__data[name] = (default() if default else _UNINITIALIZED, meta)
             io.init_json(path, default)
             watcher.watch(path, self.__sync_registered, meta=meta)
+
+
+def drill_tp(tp: Any, reactive_allowed: bool) -> Context | ContextMap | None:
+    origin = get_origin(tp) or tp
+    args = get_args(tp)
+
+    if isclass(origin) and issubclass(origin, Reactive):
+        if not reactive_allowed:
+            raise TypeError("Cannot break the reactivity chain.")
+        if issubclass(origin, SyncModel):
+            ctx = getattr(origin, "__syncwave_ctx__", None)
+            if ctx is None:
+                raise TypeError(
+                    f"'{origin.__name__}' is a SyncModel but has no context. "
+                    f"Ensure dependent models are made reactive first."
+                )
+            return ctx
+        if issubclass(origin, SyncDict):
+            return _get_sync_dict_ctx(tp)
+        if issubclass(origin, SyncList):
+            return _get_sync_list_ctx(tp)
+        if issubclass(origin, SyncSet):
+            return _get_sync_set_ctx(tp)
+        raise TypeError(f"Unknown reactive type: {origin.__name__}")  # shouldn't happen
+
+    if origin is Annotated:
+        if len(args) < 1:
+            raise ValueError("Annotated must have arguments.")
+        return drill_tp(args[0], reactive_allowed)
+
+    if origin is Union or str(origin) == "typing.Union":
+        return _get_union_ctx(args, reactive_allowed)
+
+    for arg in args:
+        drill_tp(arg, reactive_allowed=False)
+
+    return None
+
+
+def _get_sync_dict_ctx(tp: type[SyncDict[KT, VT]]) -> SyncDictCtx[KT, VT]:
+    args = get_args(tp)
+    len_args = len(args)
+
+    if len_args == 2:
+        vt = args[1]
+        inner_ctx = drill_tp(vt, reactive_allowed=True)
+        inner_type_adapter = TypeAdapter(vt)
+    elif len_args == 0:
+        inner_ctx = None
+        inner_type_adapter = TypeAdapter(Any)
+    else:
+        raise TypeError("SyncDict must have 0 or 2 arguments.")
+
+    return SyncDictCtx(
+        tp=SyncDict,
+        type_adapter=TypeAdapter(tp),
+        inner_ctx=inner_ctx,
+        inner_type_adapter=inner_type_adapter,
+    )
+
+
+def _get_sync_list_ctx(tp: type[SyncList[VT]]) -> SyncListCtx[VT]:
+    args = get_args(tp)
+    len_args = len(args)
+
+    if len_args == 1:
+        vt = args[0]
+        inner_ctx = drill_tp(vt, reactive_allowed=True)
+        inner_type_adapter = TypeAdapter(vt)
+    elif len_args == 0:
+        inner_ctx = None
+        inner_type_adapter = TypeAdapter(Any)
+    else:
+        raise TypeError("SyncList must have 0 or 1 argument.")
+
+    return SyncListCtx(
+        tp=SyncList,
+        type_adapter=TypeAdapter(tp),
+        inner_ctx=inner_ctx,
+        inner_type_adapter=inner_type_adapter,
+    )
+
+
+def _get_sync_set_ctx(tp: type[SyncSet[VT]]) -> SyncSetCtx[VT]:
+    args = get_args(tp)
+    len_args = len(args)
+
+    if len_args == 1:
+        vt = args[0]
+        drill_tp(vt, reactive_allowed=False)  # will raise if reactive
+        inner_type_adapter = TypeAdapter(vt)
+    elif len_args == 0:
+        inner_type_adapter = TypeAdapter(Any)
+    else:
+        raise TypeError("SyncSet must have 0 or 1 argument.")
+
+    return SyncSetCtx(
+        tp=SyncSet,
+        type_adapter=TypeAdapter(tp),
+        inner_ctx=None,
+        inner_type_adapter=inner_type_adapter,
+    )
+
+
+def _get_union_ctx(args: tuple[Any, ...], reactive_allowed: bool) -> ContextMap | None:
+    if not args:
+        raise ValueError("Union must have arguments.")
+
+    contexts = [drill_tp(tp, reactive_allowed) for tp in args]
+    ctx_map = {ctx.tp: ctx for ctx in contexts if ctx is not None}
+
+    if not ctx_map:
+        return None
+
+    return ContextMap(ctx_map)
