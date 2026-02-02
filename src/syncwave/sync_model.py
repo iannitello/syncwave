4cd95c95b4be8e192efbf69dc6b9eb805ab79aca
Lucas Iannitello <lucas.iannitello@protonmail.com>
2026-02-02 13:33:04 -0500

    Simplify and prevent `SyncModel` instantiation

--- src/syncwave/sync_model.py
+++ src/syncwave/sync_model.py
@@ -6,9 +6,11 @@ from collections.abc import Mapping
 from dataclasses import dataclass
 from inspect import isclass
 from typing import Any, Callable, Generic, NoReturn, TypeVar, final
+from typing_extensions import Self
 
-from pydantic import BaseModel, RootModel, TypeAdapter, create_model
-from pydantic import dataclasses as pdc
+from pydantic import BaseModel, TypeAdapter
+from pydantic import GetCoreSchemaHandler as Handler
+from pydantic_core import core_schema as cs
 
 from .reactive import Context, ContextMap, Reactive, StoreRef, assert_never, mut_atomic
 
@@ -30,26 +32,32 @@ class SyncModelSupportedMeta(ABCMeta):
 
 @final
 class SyncModelSupported(metaclass=SyncModelSupportedMeta):
-    def __init_subclass__(cls: type[SyncModelSupported], /, **kwargs: Any) -> None:
+    def __init_subclass__(cls, /, **kwargs: Any) -> None:
         raise TypeError("SyncModelSupported cannot be subclassed.")
 
 
-T = TypeVar("T", bound=SyncModelSupported)
-T_BM = TypeVar("T_BM", bound=BaseModel)
-T_RM = TypeVar("T_RM", bound=RootModel)
-T_DC = TypeVar("T_DC")  # dataclass
+T = TypeVar("T")
 
 
 @dataclass(frozen=True)
 class SyncModelCtx(Generic[T], Context):
-    tp: type[SyncModel]
-    type_adapter: TypeAdapter[SyncModel[T]]
+    tp: type[T]
+    type_adapter: TypeAdapter[T]
     fields_ctx: dict[str, Context | ContextMap]
     fields_type_adapter: dict[str, TypeAdapter[Any]]
 
 
-class SyncModel(Generic[T], Reactive):
+class SyncModel(Reactive):
     __syncwave_ctx__: SyncModelCtx[T]
+    __syncwave_original_cls__: type[T]
+
+    def __new__(cls, *args: Any, **kwargs: Any) -> NoReturn:
+        raise TypeError(f"{cls.__name__} cannot be instantiated directly.")
+
+    @classmethod
+    def __syncwave_new__(cls, self: T) -> Self:
+        self.__class__ = cls
+        return self
 
     def __syncwave_init__(self, sref: StoreRef, ctx: SyncModelCtx[T]) -> None:
         object.__setattr__(self, "__syncwave_sref__", sref)
@@ -77,230 +85,93 @@ class SyncModel(Generic[T], Reactive):
                 value.__syncwave_kill__()
         object.__setattr__(self, "__syncwave_live__", False)
 
+    def __syncwave_update__(self, new: Self | Mapping[str, Any]) -> None:
+        ctx = self.__syncwave_ctx__
+        o_setattr = self.__syncwave_original_cls__.__setattr__
 
-def create_sync_model(cls: type[T], rename: bool | str = True) -> type[SyncModel[T]]:
-    cls_ = cls  # just to prevent the type checker from flagging code as unreachable
+        new = ctx.type_adapter.validate_python(new)
 
+        for name in ctx.fields_type_adapter:
+            field_ctx = ctx.fields_ctx.get(name)
+            new_value = getattr(new, name, None)
+
+            # case 1: non-reactive content type
+            if field_ctx is None:
+                o_setattr(self, name, new_value)
+            # case 2: fixed reactive content type
+            elif isinstance(field_ctx, Context):
+                old_value = getattr(self, name)  # can't be None
+                old_value.__syncwave_update__(new_value)
+                o_setattr(self, name, old_value)  # TODO: necessary?
+            # case 3: union content type
+            elif isinstance(field_ctx, ContextMap):
+                old_value = getattr(self, name, None)
+                _setattr_union(self, name, old_value, new_value, field_ctx, o_setattr)
+            else:
+                assert_never()
+
+    @mut_atomic
+    def __setattr__(self, name: str, new_value: Any) -> None:
+        ctx = self.__syncwave_ctx__
+        o_setattr = self.__syncwave_original_cls__.__setattr__
+
+        field_ta = ctx.fields_type_adapter.get(name)
+        # case for a non-model field
+        if field_ta is None:
+            o_setattr(self, name, new_value)
+            return
+
+        field_ctx = ctx.fields_ctx.get(name)
+        new_value = field_ta.validate_python(new_value)
+
+        # case 1: non-reactive content type
+        if field_ctx is None:
+            o_setattr(self, name, new_value)
+        # case 2: fixed reactive content type
+        elif isinstance(field_ctx, Context):
+            old_value = getattr(self, name)  # can't be None
+            old_value.__syncwave_update__(new_value)
+            o_setattr(self, name, old_value)
+        # case 3: union content type
+        elif isinstance(field_ctx, ContextMap):
+            old_value = getattr(self, name, None)
+            _setattr_union(self, name, old_value, new_value, field_ctx, o_setattr)
+        else:
+            assert_never()
+
+    @mut_atomic
+    def __delattr__(self, name: str) -> None:
+        o_delattr = self.__syncwave_original_cls__.__delattr__
+        old_value = getattr(self, name, None)
+        if isinstance(old_value, Reactive):
+            old_value.__syncwave_kill__()
+        o_delattr(self, name)
+
+    @classmethod
+    def __get_pydantic_core_schema__(cls, src: Any, handler: Handler) -> cs.CoreSchema:
+        original_cls = cls.__syncwave_original_cls__
+        original_schema = handler.generate_schema(original_cls)
+
+        non_instance_schema = cs.no_info_after_validator_function(
+            cls.__syncwave_new__, original_schema
+        )
+        instance_schema = cs.is_instance_schema(cls)
+        return cs.union_schema(
+            [instance_schema, non_instance_schema],
+            serialization=cs.wrap_serializer_function_ser_schema(
+                lambda v, nxt: nxt(v),
+                schema=original_schema,
+            ),
+        )
+
+
+def create_sync_model(cls: type[T], rename: bool | str = True) -> type[T]:
     cls_name = f"Sync{cls.__name__}" if rename is True else rename or cls.__name__
-
-    if issubclass(cls_, BaseModel):
-        if issubclass(cls_, RootModel):
-            return _create_root_model(cls, cls_name)
-        return _create_base_model(cls, cls_name)
-    return _create_dataclass(cls, cls_name)
-
-
-def _create_base_model(cls: type[T_BM], cls_name: str) -> type[SyncModel[T_BM]]:
-    o_setattr = cls.__setattr__
-    o_delattr = cls.__delattr__
-
-    def syncwave_update(
-        self: SyncModel[T_BM],
-        new: SyncModel[T_BM] | T_BM | Mapping[str, Any],
-    ) -> None:
-        new = self.__syncwave_ctx__.type_adapter.validate_python(new)
-
-        for name in cls.model_fields:
-            field_ctx = self.__syncwave_ctx__.fields_ctx.get(name)
-            new_value = getattr(new, name, None)
-
-            # case 1: non-reactive content type
-            if field_ctx is None:
-                o_setattr(self, name, new_value)
-            # case 2: fixed reactive content type
-            elif isinstance(field_ctx, Context):
-                old_value = getattr(self, name)  # can't be None
-                old_value.__syncwave_update__(new_value)
-                o_setattr(self, name, old_value)
-            # case 3: union content type
-            elif isinstance(field_ctx, ContextMap):
-                old_value = getattr(self, name, None)
-                _setattr_union(self, name, old_value, new_value, field_ctx, o_setattr)
-            else:
-                assert_never()
-
-    @mut_atomic
-    def new_setattr(self: SyncModel[T_BM], name: str, new_value: Any) -> None:
-        field_ta = self.__syncwave_ctx__.fields_type_adapter.get(name)
-        # case for a non-model field
-        if field_ta is None:
-            o_setattr(self, name, new_value)
-            return
-
-        field_ctx = self.__syncwave_ctx__.fields_ctx.get(name)
-        new_value = field_ta.validate_python(new_value)
-
-        # case 1: non-reactive content type
-        if field_ctx is None:
-            o_setattr(self, name, new_value)
-        # case 2: fixed reactive content type
-        elif isinstance(field_ctx, Context):
-            old_value = getattr(self, name)  # can't be None
-            old_value.__syncwave_update__(new_value)
-            o_setattr(self, name, old_value)
-        # case 3: union content type
-        elif isinstance(field_ctx, ContextMap):
-            old_value = getattr(self, name, None)
-            _setattr_union(self, name, old_value, new_value, field_ctx, o_setattr)
-        else:
-            assert_never()
-
-    @mut_atomic
-    def new_delattr(self: SyncModel[T_BM], name: str) -> None:
-        old_value = getattr(self, name, None)
-        if isinstance(old_value, Reactive):
-            old_value.__syncwave_kill__()
-        o_delattr(self, name)
-
-    new_cls = create_model(cls_name, __base__=(cls, SyncModel))
-    new_cls.__syncwave_update__ = syncwave_update
-    new_cls.__setattr__ = new_setattr
-    new_cls.__delattr__ = new_delattr
-    new_cls.__module__ = cls.__module__
-    new_cls.__abstractmethods__ = frozenset()
-    return new_cls
-
-
-def _create_root_model(cls: type[T_RM], cls_name: str) -> type[SyncModel[T_RM]]:
-    o_setattr = cls.__setattr__
-    o_delattr = cls.__delattr__
-
-    def syncwave_update(
-        self: SyncModel[T_RM],
-        new: SyncModel[T_RM] | T_RM | Any,
-    ) -> None:
-        new = self.__syncwave_ctx__.type_adapter.validate_python(new)
-
-        field_ctx = self.__syncwave_ctx__.fields_ctx.get("root")
-        new_value = new.root
-
-        # case 1: non-reactive content type
-        if field_ctx is None:
-            o_setattr(self, "root", new_value)
-        # case 2: fixed reactive content type
-        elif isinstance(field_ctx, Context):
-            old_value = self.root  # can't be None
-            old_value.__syncwave_update__(new_value)
-            o_setattr(self, "root", old_value)
-        # case 3: union content type
-        elif isinstance(field_ctx, ContextMap):
-            old_value = getattr(self, "root", None)
-            _setattr_union(self, "root", old_value, new_value, field_ctx, o_setattr)
-        else:
-            assert_never()
-
-    @mut_atomic
-    def new_setattr(self: SyncModel[T_RM], name: str, new_value: Any) -> None:
-        if name != "root":
-            o_setattr(self, name, new_value)
-            return
-
-        field_ctx = self.__syncwave_ctx__.fields_ctx.get("root")
-        root_ta = self.__syncwave_ctx__.fields_type_adapter["root"]
-        new_value = root_ta.validate_python(new_value)
-
-        # case 1: non-reactive content type
-        if field_ctx is None:
-            o_setattr(self, "root", new_value)
-        # case 2: fixed reactive content type
-        elif isinstance(field_ctx, Context):
-            old_value = self.root  # can't be None
-            old_value.__syncwave_update__(new_value)
-            o_setattr(self, "root", old_value)
-        # case 3: union content type
-        elif isinstance(field_ctx, ContextMap):
-            old_value = getattr(self, "root", None)
-            _setattr_union(self, "root", old_value, new_value, field_ctx, o_setattr)
-        else:
-            assert_never()
-
-    @mut_atomic
-    def new_delattr(self: SyncModel[T_RM], name: str) -> None:
-        old_value = getattr(self, name, None)
-        if isinstance(old_value, Reactive):
-            old_value.__syncwave_kill__()
-        o_delattr(self, name)
-
-    new_cls = create_model(cls_name, __base__=(cls, SyncModel))
-    new_cls.__syncwave_update__ = syncwave_update
-    new_cls.__setattr__ = new_setattr
-    new_cls.__delattr__ = new_delattr
-    new_cls.__module__ = cls.__module__
-    new_cls.__abstractmethods__ = frozenset()
-    return new_cls
-
-
-def _create_dataclass(cls: type[T_DC], cls_name: str) -> type[SyncModel[T_DC]]:
-    if not pdc.is_pydantic_dataclass(cls):
-        cls = pdc.dataclass(cls)
-
-    o_setattr = cls.__setattr__
-    o_delattr = cls.__delattr__
-
-    def syncwave_update(
-        self: SyncModel[T_DC],
-        new: SyncModel[T_DC] | T_DC | Mapping[str, Any],
-    ) -> None:
-        new = self.__syncwave_ctx__.type_adapter.validate_python(new)
-
-        for name in cls.__pydantic_fields__:
-            field_ctx = self.__syncwave_ctx__.fields_ctx.get(name)
-            new_value = getattr(new, name, None)
-
-            # case 1: non-reactive content type
-            if field_ctx is None:
-                o_setattr(self, name, new_value)
-            # case 2: fixed reactive content type
-            elif isinstance(field_ctx, Context):
-                old_value = getattr(self, name)  # can't be None
-                old_value.__syncwave_update__(new_value)
-                o_setattr(self, name, old_value)
-            # case 3: union content type
-            elif isinstance(field_ctx, ContextMap):
-                old_value = getattr(self, name, None)
-                _setattr_union(self, name, old_value, new_value, field_ctx, o_setattr)
-            else:
-                assert_never()
-
-    @mut_atomic
-    def new_setattr(self: SyncModel[T_DC], name: str, new_value: Any) -> None:
-        field_ta = self.__syncwave_ctx__.fields_type_adapter.get(name)
-        # case for a non-model field
-        if field_ta is None:
-            o_setattr(self, name, new_value)
-            return
-
-        field_ctx = self.__syncwave_ctx__.fields_ctx.get(name)
-        new_value = field_ta.validate_python(new_value)
-
-        # case 1: non-reactive content type
-        if field_ctx is None:
-            o_setattr(self, name, new_value)
-        # case 2: fixed reactive content type
-        elif isinstance(field_ctx, Context):
-            old_value = getattr(self, name)  # can't be None
-            old_value.__syncwave_update__(new_value)
-            o_setattr(self, name, old_value)
-        # case 3: union content type
-        elif isinstance(field_ctx, ContextMap):
-            old_value = getattr(self, name, None)
-            _setattr_union(self, name, old_value, new_value, field_ctx, o_setattr)
-        else:
-            assert_never()
-
-    @mut_atomic
-    def new_delattr(self: SyncModel[T_DC], name: str) -> None:
-        old_value = getattr(self, name, None)
-        if isinstance(old_value, Reactive):
-            old_value.__syncwave_kill__()
-        o_delattr(self, name)
-
-    new_cls = type(cls_name, (cls, SyncModel), {"__module__": cls.__module__})
-    new_cls.__syncwave_update__ = syncwave_update
-    new_cls.__setattr__ = new_setattr
-    new_cls.__delattr__ = new_delattr
-    new_cls.__abstractmethods__ = frozenset()
-    return pdc.dataclass(new_cls)
+    return type(
+        cls_name,
+        (SyncModel, cls),
+        {"__module__": cls.__module__, "__syncwave_original_cls__": cls},
+    )
 
 
 def _setattr_union(
@@ -317,7 +188,7 @@ def _setattr_union(
 
     if old_is_reactive and new_is_reactive and same_type:
         old_value.__syncwave_update__(new_value)
-        original_setattr(self, field_name, old_value)
+        original_setattr(self, field_name, old_value)  # TODO: necessary?
     else:
         if old_is_reactive:
             old_value.__syncwave_kill__()
