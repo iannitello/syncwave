0f5c1b0d20c17bac248647fb3cd246f8bbf1ffb8
Lucas Iannitello <lucas.iannitello@protonmail.com>
2026-03-24 18:34:45 -0400

    Update version

--- src/syncwave/__init__.py
+++ src/syncwave/__init__.py
@@ -1,6 +1,6 @@
 """Turn plain JSONs into live data stores, two-way synced with Python objects."""
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 
 from .reactive import DeadReferenceError, Reactive
